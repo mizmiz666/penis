ebbb2efde001ac7de909628c6ec4e7b00cd59863
mizmiz666 <mojoes2@hotmail.fr>
2013-06-13 02:19:09 +0100

    Silly bash script for fun.

---
+++ penis.sh
@@ -0,0 +1,52 @@
+# JERK OFF
+
+INTERVAL=1 # The speed at which you're masturbating. It depends on people and experience
+COUNT="0"  
+echo ""
+
+while true 
+do
+	COUNT=$(( COUNT + 1 ))
+
+	case $COUNT in 
+		"1") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8===jjj=D   "
+			sleep $INTERVAL
+			;;
+		"2") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8==jjj==D   "
+			sleep $INTERVAL
+			;;
+		"3") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8===jjj=D   "
+			sleep $INTERVAL
+			;;
+		"4") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8==jjj==D   "
+			sleep $INTERVAL
+			;;
+		"5") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D~~~"
+			sleep $INTERVAL
+			;;
+
+		"6") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D   "
+			sleep $INTERVAL
+			;;
+		"7") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D~~~"
+			sleep $INTERVAL
+			;;
+		"8") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D   "
+			sleep $INTERVAL
+			;;
+		"9") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D~~~"
+			sleep $INTERVAL
+			;;
+
+		"10") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D   "
+			sleep $INTERVAL
+			;;
+		"11") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D~~~"
+			sleep $INTERVAL
+			;;
+		"12") printf "%s\b\b\b\b\b\b\b\b\b\b\b\b""8=======D   "
+			sleep $INTERVAL
+			;;
+		*) COUNT="0" ;;
+	esac
+done
